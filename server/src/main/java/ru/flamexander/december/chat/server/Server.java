6553d739b61af92107da364fc3ddc155a83df78c
vvltaranenko <tvvchaik@mail.ru>
2024-01-14 22:23:08 +0300

    homework22 add message: user not found

--- server/src/main/java/ru/flamexander/december/chat/server/Server.java
+++ server/src/main/java/ru/flamexander/december/chat/server/Server.java
@@ -48,12 +48,18 @@ public class Server {
     }
 
     public synchronized void sendPrivateMessage(ClientHandler sender, String receiverUsername, String message) {
-        // TODO homework.
+        // TODO homework
+        boolean receiverNotFound = true;
         for (ClientHandler clientHandler : clients) {
             if(clientHandler.getUsername().equals(receiverUsername)) {
                 clientHandler.sendMessage("Private Message from " + sender.getUsername() + " to " + receiverUsername + ": " + message);
                 sender.sendMessage("Private Message from " + sender.getUsername() + " to " + receiverUsername + ": " + message);
+                receiverNotFound = false;
             }
+        }
+
+        if (receiverNotFound) {
+            sender.sendMessage("User: " + receiverUsername + " not found");
         }
     }
 }
